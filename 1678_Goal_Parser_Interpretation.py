ba60bcc8688d2569507e75cee34dbccbf3dd6a91
SABIN K SANTHOSH <sabinksanthosh6@gmail.com>
2025-05-07 08:10:26 +0530

    1678. Goal Parser Interpretation using startswith()

--- 1678_Goal_Parser_Interpretation.py
+++ 1678_Goal_Parser_Interpretation.py
@@ -1,4 +1,19 @@
 class Solution(object):
     def interpret(self, command):
-        return command.replace('()', 'o').replace('(al)', 'al')
+     result = ""
+     i = 0
     
+     while i < len(command):
+         if command[i] == 'G':
+             result += 'G'
+             i += 1
+         elif command.startswith("()", i):
+             result += 'o'
+             i += 2
+         elif command.startswith("(al)", i):
+             result += 'al'
+             i += 4
+         else:
+             i += 1  # Skip unrecognized characters (shouldn't happen per problem constraints)
+    
+     return result
