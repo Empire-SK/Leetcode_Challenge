0b4aa3c33a014090d41b07c2ba263e0fac5e8712
SABIN K SANTHOSH <sabinksanthosh6@gmail.com>
2025-06-01 14:32:24 +0530

    solution using two pointers(left,right)

--- 977_Squares_of_a_Sorted_Array.py
+++ 977_Squares_of_a_Sorted_Array.py
@@ -1,3 +1,17 @@
 class Solution(object):
     def sortedSquares(self, nums):
-        return sorted([num * num for num in nums])
+        n = len(nums)
+        result = [0] * n
+        left, right = 0, n - 1
+        index = n - 1
+        
+        while left <= right:
+            if abs(nums[left]) > abs(nums[right]):
+                result[index] = nums[left] ** 2
+                left += 1
+            else:
+                result[index] = nums[right] ** 2
+                right -= 1
+            index -= 1  
+        
+        return result  
