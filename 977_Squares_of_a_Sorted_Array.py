cf5d26b774a967bcd8d07cba29333376babbb8da
SABIN K SANTHOSH <sabinksanthosh6@gmail.com>
2025-06-01 14:33:02 +0530

    another method

--- 977_Squares_of_a_Sorted_Array.py
+++ 977_Squares_of_a_Sorted_Array.py
@@ -1,17 +1,6 @@
 class Solution(object):
     def sortedSquares(self, nums):
-        n = len(nums)
-        result = [0] * n
-        left, right = 0, n - 1
-        index = n - 1
-        
-        while left <= right:
-            if abs(nums[left]) > abs(nums[right]):
-                result[index] = nums[left] ** 2
-                left += 1
-            else:
-                result[index] = nums[right] ** 2
-                right -= 1
-            index -= 1  
-        
-        return result  
+        x=[]
+        for i in range(len(nums)):
+            x.append(nums[i]**2)
+        return sorted(x)
