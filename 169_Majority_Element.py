3291e35c9665e2c6f9ba1284969e2454fa1c3c70
SABIN K SANTHOSH <sabinksanthosh6@gmail.com>
2025-06-06 01:36:25 +0530

    another less runtime method

--- 169_Majority_Element.py
+++ 169_Majority_Element.py
@@ -1,10 +1,21 @@
 class Solution(object):
     def majorityElement(self, nums):
-        candidate, count = None, 0
+        majority=nums[0]
+        count=0
 
-        for num in nums:
-            if count == 0:
-                candidate = num
-            count += (1 if num == candidate else -1)
-        
-        return candidate
+        for x in nums:
+            if count== 0:
+                majority=x
+
+            if x==majority:
+                count +=1
+            else:
+                count -=1
+
+        count=0
+        for x in nums:
+            if x == majority:
+                count +=1
+
+        if count>len(nums)//2:
+            return majority
