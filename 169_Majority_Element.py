1ab989c3d70eb9cb2f70ec69fc6064447ac15fbf
SABIN K SANTHOSH <sabinksanthosh6@gmail.com>
2025-06-06 01:35:36 +0530

    Optimized Approach: Boyer-Moore Voting Algorithm

--- 169_Majority_Element.py
+++ 169_Majority_Element.py
@@ -1,29 +1,10 @@
 class Solution(object):
     def majorityElement(self, nums):
-        candidate = -1
-        votes = 0  # Fixed variable name
+        candidate, count = None, 0
 
-        # Finding majority candidate
-        for i in range(len(nums)):  
-            if votes == 0:
-                candidate = nums[i]  # Fixed array name
-                votes = 1
-            else:
-                if nums[i] == candidate:  # Fixed array name
-                    votes += 1
-                else:
-                    votes -= 1
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
         
-        count = 0
-
-        # Checking if majority candidate occurs more than n/2
-        # times
-
-        for i in range(len(nums)):  
-            if nums[i] == candidate:  # Fixed array name
-                count += 1
-            
-        if count > len(nums) // 2:  # Fixed condition (used len(nums) instead of n)
-            return candidate
-        else:
-            return -1
+        return candidate
