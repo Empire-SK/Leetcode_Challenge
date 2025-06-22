5a4a8c3941b2924747795f312dc470870ebd7bd2
Empire <sabinksanthosh6@gmail.com>
2025-06-22 15:19:02 +0530

    splited into words and remove the last value of each element in the sorted list

---
+++ 26_Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+
+        write = 1  # Pointer to place the next unique element
+
+        for read in range(1, len(nums)):
+            if nums[read] != nums[read - 1]:
+                nums[write] = nums[read]
+                write += 1
+
+        return write
--- Automatic_Filecreater.py
+++ Automatic_Filecreater.py
@@ -16,4 +16,4 @@ def fixer(text):
     print(f"{filename} created successfully!")
 
 
-fixer("26. Remove Duplicates from Sorted Array")
+fixer("1859. Sorting the Sentence")
