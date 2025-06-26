0f5024a9b6d4068cfe3ed6b05ee7f4467e7e6367
Empire <sabinksanthosh6@gmail.com>
2025-06-26 23:23:36 +0530

    350. Intersection of Two Arrays II

---
+++ 350_Intersection_of_Two_Arrays_II.py
@@ -0,0 +1,12 @@
+import 
+class Solution:
+    def intersect(self, nums1, nums2):
+        if len(nums1) > len(nums2): return self.intersect(nums2, nums1)
+            
+        cnt = Counter(nums1)
+        ans = []
+        for x in nums2:
+            if cnt[x] > 0:
+                ans.append(x)
+                cnt[x] -= 1
+        return ans
--- Automatic_Filecreater.py
+++ Automatic_Filecreater.py
@@ -16,4 +16,4 @@ def fixer(text):
     print(f"{filename} created successfully!")
 
 
-fixer("349. Intersection of Two Arrays")
+fixer("350. Intersection of Two Arrays II")
